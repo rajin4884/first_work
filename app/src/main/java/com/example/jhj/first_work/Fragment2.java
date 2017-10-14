9f2083830ea1d8776a888853d3989b2c5d672eb5
rajin4884 <rajin4884@gmail.com>
2017-10-15 08:03:09 +0900

    세번째 올리기

--- app/src/main/java/com/example/jhj/first_work/Fragment2.java
+++ app/src/main/java/com/example/jhj/first_work/Fragment2.java
@@ -1,10 +1,12 @@
 package com.example.jhj.first_work;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -12,6 +14,7 @@ import android.view.ViewGroup;
  */
 public class Fragment2 extends Fragment {
 
+    Intent intent;
 
     public Fragment2() {
         // Required empty public constructor
@@ -22,7 +25,20 @@ public class Fragment2 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment2, container, false);
+//        return inflater.inflate(R.layout.fragment_fragment2, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_fragment2, container, false);
+
+        intent = new Intent(getActivity(), SceneTransitionsActivity.class);
+        final Button button = (Button) rootView.findViewById(R.id.btn4);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+
+        return rootView;
     }
 
 
---
+++ app/src/main/java/com/example/jhj/first_work/SceneTransitionsActivity.java
@@ -0,0 +1,68 @@
+package com.example.jhj.first_work;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.transition.Scene;
+
+import android.transition.Transition;
+import android.transition.TransitionInflater;
+import android.transition.TransitionManager;
+import android.view.ViewGroup;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class SceneTransitionsActivity extends AppCompatActivity {
+
+    ViewGroup rootContainer;
+    Scene scene1;
+    Scene scene2;
+    Scene scene3;
+    Transition transitionMgr;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scene_transitions);
+
+        rootContainer =
+                (ViewGroup) findViewById(R.id.rootContainer);
+
+        transitionMgr = TransitionInflater.from(this)
+                .inflateTransition(R.transition.transition);
+
+        scene1 = Scene.getSceneForLayout(rootContainer,
+                R.layout.scene1_layout, this);
+
+        scene2 = Scene.getSceneForLayout(rootContainer,
+                R.layout.scene2_layout, this);
+
+        scene3 = Scene.getSceneForLayout(rootContainer,
+                R.layout.scene3_layout, this);
+
+        scene1.enter();
+
+    }
+
+    public void goToScene2 (View view)
+    {
+        TransitionManager.go(scene2, transitionMgr);
+        //Toast.makeText(getApplicationContext(),
+        //      "장면 전환 2에 도달함", Toast.LENGTH_LONG).show();
+
+    }
+
+    public void goToScene1 (View view)
+    {
+        TransitionManager.go(scene1, transitionMgr);
+        //Toast.makeText(getApplicationContext(),
+        //      "장면 전환 1에 도달함", Toast.LENGTH_LONG).show();
+    }
+
+    public void goToScene3 (View view)
+    {
+        TransitionManager.go(scene3, transitionMgr);
+        //Toast.makeText(getApplicationContext(),
+        //      "장면 전환 3에 도달함", Toast.LENGTH_LONG).show();
+    }
+}
